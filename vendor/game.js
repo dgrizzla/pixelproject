2d4c640708136093a93c6bdb223593ae88b689ab
dgrizzla <dylandau5@gmail.com>
2016-04-13 00:07:07 -0600

    new sprites added

--- vendor/game.js
+++ vendor/game.js
@@ -1,41 +1,56 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
-  preload: preload,
-  create: create,
-  update: update
-});
-var aagns = ['aang', 'aang2', 'aang3'];
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-game', { preload: preload, create: create, update: update});
+
+
 var index = 0;
+var arrHair = ['hair0','hair1','hair2'];
 
 function preload() {
-  game.load.image('aang', 'assets/aang.png');
-  game.load.image('aang2', 'assets/aang2.png');
-  game.load.image('aang3', 'assets/aang3.png');
-  game.load.image('btnleft', 'assets/left.png');
-  game.load.image('btnright', 'assets/right.png');
+  game.load.image('body1','assets/body/body1.png');
+  game.load.image('btnleft','assets/left.png');
+  game.load.image('btnright','assets/right.png');
+  game.load.image('hair0','assets/hair/hair0.png');
+  game.load.image('hair1','assets/hair/hair1.png');
+  game.load.image('hair2','assets/hair/hair2.png');
+  game.load.image('top1','assets/top/top1.png')
+  game.load.image('bottom1','assets/bottom/bottom1.png');
 }
-var aang, left, right, containerPlayer;
 
-function onLeft() {
+var hair,left,right;
+
+function onLeftHair() {
   if (index <= 0) {
     index = 3;
   }
   index--;
-  aang.loadTexture(aagns[index]);
+  hair.loadTexture(arrHair[index]);
 }
 
-function onRight() {
+function onRightHair() {
   if (index >= 2) {
     index = -1;
   }
   index++;
-  aang.loadTexture(aagns[index]);
+  hair.loadTexture(arrHair[index]);
 }
 
 function create() {
   game.stage.backgroundColor = "#4488AA";
-  aang = game.add.sprite(10, 10, aagns[index]);
-  left = game.add.button(100, 250, 'btnleft', onLeft).scale.setTo(0.8, 0.8);
-  right = game.add.button(380, 250, 'btnright', onRight).scale.setTo(0.8, 0.8);
+
+  game.add.image(150,75,'body1');
+
+  hair = game.add.sprite(141,-30,arrHair[index]);
+  game.add.image(141,66,'top1');
+  game.add.image(150,93,'bottom1');
+
+  hairLeft = game.add.button(247,130,'btnleft',onLeftHair).scale.setTo(0.8);
+  hairRight = game.add.button(410,130,'btnright',onRightHair).scale.setTo(0.8);
+
+  game.add.image(247,250,'btnleft').scale.setTo(0.8);
+  game.add.image(410,250,'btnright').scale.setTo(0.8);
+
+  game.add.image(247,370,'btnleft').scale.setTo(0.8);
+  game.add.image(410,370,'btnright').scale.setTo(0.8);
+
 }
 
 function update() {}
